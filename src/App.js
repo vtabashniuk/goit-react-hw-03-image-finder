afed932b284144e8a877b1df3e477409ce362f51
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-02-13 22:11:42 +0200

    Добавляет спинер во время загрузки

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import getImages from "./api/getImages";
 import Searchbar from "./Searchbar";
 import ImageGallery from "./ImageGallery";
+import { BallTriangle } from "react-loader-spinner";
 
 const INITIAL_STATE = {
   imagesCollection: [],
   currentPage: 1,
   searchQuery: "",
+  isLoading: false,
 };
 
 class App extends Component {
@@ -14,6 +16,7 @@ class App extends Component {
     imagesCollection: [],
     currentPage: 1,
     searchQuery: "",
+    isLoading: false,
   };
 
   formSubmitHandler = (query) => {
@@ -22,12 +25,15 @@ class App extends Component {
 
   fetchImages = () => {
     const { searchQuery, currentPage } = this.state;
-    getImages(searchQuery, currentPage).then((data) => {
-      this.setState((prevState) => ({
-        imagesCollection: [...prevState.imagesCollection, ...data],
-        currentPage: prevState.currentPage + 1,
-      }));
-    });
+    this.setState({ isLoading: true });
+    getImages(searchQuery, currentPage)
+      .then((data) => {
+        this.setState((prevState) => ({
+          imagesCollection: [...prevState.imagesCollection, ...data],
+          currentPage: prevState.currentPage + 1,
+        }));
+      })
+      .finally(() => this.setState({ isLoading: false }));
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -40,6 +46,14 @@ class App extends Component {
     return (
       <>
         <h1 className="title">Homework 3</h1>
+        {this.state.isLoading && (
+          <BallTriangle
+            heigth="100"
+            width="100"
+            color="grey"
+            ariaLabel="loading-indicator"
+          />
+        )}
         <Searchbar onSubmit={this.formSubmitHandler} />
         <ImageGallery
           images={this.state.imagesCollection}
